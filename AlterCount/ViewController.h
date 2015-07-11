46956bd967e27b7833d6dfcfff3789aac0539952
chenmalixue <xuanchen@chenxuandeMacBook-Pro.local>
2015-07-11 13:56:09 +0800

    Initial Commit

---
+++ AlterCount/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlterCount
+//
+//  Created by 陈宣 on 15/7/11.
+//  Copyright (c) 2015年 陈宣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AlterCount/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AlterCount
+//
+//  Created by 陈宣 on 15/7/11.
+//  Copyright (c) 2015年 陈宣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
